5ad688ecb2311460db4a651817a6c12dd8a9bb2a
honboubao <christian.tu@gmx.at>
2024-11-22 13:34:35 +0100

    print_mem utils function for debugging

--- misc/utils.py
+++ misc/utils.py
@@ -1,5 +1,13 @@
+import gc
+
 def find(list, condition, default=None):
     try:
         return next(filter(condition, list))
     except StopIteration:
         return default
+
+def print_mem():
+    gc.collect()
+    used = gc.mem_alloc()
+    free = gc.mem_free()
+    print("Memory used: " + str(used) + " (" + str(round(100 * used / (used + free))) + "%) free: " + str(free))
